7a12c279cb36187938d05be033d245339caa6256
Ulises <diseno.aruma@gmail.com>
2019-11-11 20:38:32 -0600

    Cambio en el login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,29 +11,50 @@
 
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700&amp;subset=latin" rel="stylesheet">
     <link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/argon.css" rel="stylesheet">
     <link href="css/nifty.min.css" rel="stylesheet">
     <link href="premium/icon-sets/icons/line-icons/premium-line-icons.min.css" rel="stylesheet">
     <link href="premium/icon-sets/icons/solid-icons/premium-solid-icons.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="assets/css/argon.css?v=1.1.0" type="text/css">
+	<link rel="stylesheet" href="argon.css">
+	  <link rel="stylesheet" href="assets/vendor/@fortawesome/fontawesome-free/css/all.min.css" type="text/css">
     {{--<link href="css/pace.min.css" rel="stylesheet">
     <script src="js/pace.min.js"></script>
+	
     <link href="temp.css" rel="stylesheet"> --}}
 </head>
 
-<body>
-    <div id="container" class="cls-container">
-		<div class="cls-content">
-		    <div class="cls-content-sm panel">
-		        <div class="panel-body">
-		        	<div class="media-body text-bold text-main">
-	                    <img width="100%" src="/img/logos/logo_full.png">
-	                </div>
+<body style=background-image:url(/img/bg-img/gris.jpg);">
+</body>
 
+    <div class="main-content"> 
+    <!-- Header -->
+    <div class="header py-7 py-lg-8 pt-lg-9">
+      <div class="container">
+          <div class="row justify-content-center">
+            <div class="container">
+             
+    </div>
+    <!-- Page content -->
+    <div class="container mt--1 pb-8">
+      <div class="row justify-content-center">
+        <div class="col-md-7 col-lg-5">
+          <div class="card bg-secondary border-0 mb-0">
+            <div class="card-header bg- pb-5">
+              <div class="btn-wrapper text-center">
+              <img src="/img/logos/logo.png" class="img-responsive">
+              </div>
+            </div>
+            <div class="card-body px-lg-7 py-lg-5">
+              <div class="text-center text-muted mb-4">					
+					<div class="text-center text-muted mb-4">
+		                <h1 style="font-size:40px">Senderos</h1>
+		                <p style="font-size:20px">Ingrese sus credenciales</p>
+		            
+	                </div>
 		        	<div class="media pad-top bord-top"></div>
 
-		            <div class="mar-ver pad-btm">
-		                <h1 class="h3">Senderos</h1>
-		                <p>Ingrese sus credenciales</p>
-		            </div>
+		            
 		            @if(Session::has('info'))
 						<div class="row">
 						    <div class="panel-heading">
@@ -51,12 +72,12 @@
                 		<input type="hidden" id="long" name="long">
 		            	<input type="hidden" name="_token" value="{{ csrf_token() }}">
 		                <div class="form-group">
-		                    <input name="email" type="text" class="form-control" placeholder="Usuario" autofocus value="{{ old('email') }}">
+		                    <input name="email" type="text" class="form-control" placeholder="Usuario" autofocus value="{{ old('email') }}"> 
 		                </div>
+						
 		                <div class="form-group">
 		                    <input name="password" type="password" class="form-control" placeholder="Contraseña">
 		                </div>
-
 		                {{--<div class="checkbox pad-btm text-left">
 		                    <input id="demo-form-checkbox" class="magic-checkbox" type="checkbox">
 		                    <label for="demo-form-checkbox">Recordarme</label>
@@ -86,5 +107,7 @@
     </div>
 
     <script src="/js/jquery.min.js"></script>
+	<script src="/js/argon.js"></script>
+	<script src="/js/argon.min.js"></script>
 </body>
 </html>
