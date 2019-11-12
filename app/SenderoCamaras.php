2350042fbff8e7ac0b697f75da998983ed1fef74
Carlos Gonzalez <greenthumb.1381@gmail.com>
2019-11-11 19:05:54 -0600

    Capas

--- app/SenderoCamaras.php
+++ app/SenderoCamaras.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class SenderoCamaras extends Model
 {
     protected $connection = 'map';
-    protected $table = 'senderocamaras';
+    protected $table = 'senderoscamaras';
 }
