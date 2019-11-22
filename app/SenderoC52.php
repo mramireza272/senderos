b10274b8aeb1ba97a447a851e615d1c43762861f
Martin Ramirez <mramireza272@gmail.com>
2019-11-21 19:33:26 -0600

    issues solicitados por Veronica

---
+++ app/SenderoC52.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SenderoC52 extends Model
+{
+    //
+}
---
+++ app/SenderoIztapalapa.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SenderoIztapalapa extends Model
+{
+    protected $connection = 'map';
+    protected $table = 'sendero_iztapalapa';
+}
